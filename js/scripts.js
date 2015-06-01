73e0791dff31330f319cc5d117cb9c500a0ce33c
Guest User <student@epicodus.com>
2015-06-01 12:18:48 -0700

    add everything

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("form#new-location").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedCity = $("input#new-city").val();
+    var inputtedCountry = $("input#new-country").val();
+    var inputtedSeason = $("#new-season").val();
+
+    var newLocation = { city: inputtedCity, country: inputtedCountry, season: inputtedSeason
+    };
+
+    $("ul#locations").append("<li><span class='location'>" + newLocation.city + "</span></li>");
+
+    $(".location").last().click(function() {
+      $("#show-location").show();
+
+      $("show-location h2").text(newLocation.city);
+      $(".city").text(newLocation.city);
+      $(".country").text(newLocation.country);
+      $(".season").text(newLocation.season);
+    });
+
+    $("input#new-city").val("");
+    $("input#new-country").val("");
+    $("#new-season").val("");
+
+  });
+});
